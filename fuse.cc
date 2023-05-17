f83f8957845ee6871f3d24bd006736e6f5a18dbc
ioli <bipinoli90@gmail.com>
2023-05-17 16:55:06 +0200

    open handler

--- fuse.cc
+++ fuse.cc
@@ -248,12 +248,7 @@ void
 fuseserver_open(fuse_req_t req, fuse_ino_t ino,
      struct fuse_file_info *fi)
 {
-  // You fill this in
-#if 0
   fuse_reply_open(req, fi);
-#else
-  fuse_reply_err(req, ENOSYS);
-#endif
 }
 
 void
