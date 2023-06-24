c8aed1af8869c31f1e062ce027ca03d84a727271
ioli <bipinoli90@gmail.com>
2023-06-24 16:12:16 +0200

    lab 5 - rearchitected lock caching based on new observations and old design

--- lock_client_cache.cc
+++ lock_client_cache.cc
@@ -32,7 +32,11 @@ lock_client_cache::lock_client_cache(std::string xdst,
   id = host.str();
   last_port = rlock_port;
   rpcs *rlsrpc = new rpcs(rlock_port);
+
   /* register RPC handlers with rlsrpc */
+  rlsrpc->reg(rlock_protocol::revoke, this, &lock_client_cache::revoke_handler);
+  rlsrpc->reg(rlock_protocol::retry, this, &lock_client_cache::retry_handler);
+
   pthread_t th;
   int r = pthread_create(&th, NULL, &releasethread, (void *) this);
   assert (r == 0);
@@ -40,26 +44,140 @@ lock_client_cache::lock_client_cache(std::string xdst,
 
 
 void
-lock_client_cache::releaser()
-{
-
+lock_client_cache::releaser() {
   // This method should be a continuous loop, waiting to be notified of
   // freed locks that have been revoked by the server, so that it can
   // send a release RPC.
+  while (true) {
+    auto req = release_queue.consume();
+    while (true) {
+      pthread_mutex_lock(&cache_mutex);
+      Lock &lock = cache[req.lid];
+      if (lock.status == Lock::LOCKED) {
+        pthread_cond_wait(&released_to_cache_signal, &cache_mutex);
+        continue;
+      }
+      if (lock.status == Lock::ACQUIRING) {
+        pthread_cond_wait(&acquired_signal, &cache_mutex);
+        continue;
+      }
+      if (lock.status == Lock::NONE || lock.status == Lock::RELEASING) {
+        pthread_mutex_unlock(&cache_mutex);
+        break;
+      }
 
+      lock.status = Lock::RELEASING;
+      pthread_mutex_unlock(&cache_mutex);
 
+      int r; assert(cl->call(lock_protocol::release, cl->id(), req.lid, req.seq, r) == lock_protocol::OK);
+
+      pthread_mutex_lock(&cache_mutex);
+      lock.status = Lock::NONE;
+      pthread_mutex_unlock(&cache_mutex);
+      pthread_cond_broadcast(&released_to_server_signal);
+      break;
+    }
+  }
 }
+
 
 
 lock_protocol::status
-lock_client_cache::acquire(lock_protocol::lockid_t lid)
-{
-  return lock_protocol::RPCERR;
+lock_client_cache::acquire(lock_protocol::lockid_t lid) {
+  ScopedLock guard(&cache_mutex);
+
+  Lock& lock = cache[lid];
+
+  while (true) {
+    if (lock.status == Lock::LOCKED) {
+      pthread_cond_wait(&released_to_cache_signal, &cache_mutex);
+      continue;
+    }
+    if (lock.status == Lock::ACQUIRING) {
+      pthread_cond_wait(&acquired_signal, &cache_mutex);
+      continue;
+    }
+    if (lock.status == Lock::RELEASING) {
+      pthread_cond_wait(&released_to_server_signal, &cache_mutex);
+      continue;
+    }
+    if (lock.status == Lock::FREE) {
+      lock.status = Lock::LOCKED;
+      return lock_protocol::OK;
+    }
+
+    lock.status = Lock::ACQUIRING;
+    auto seq = ++lock.seqnum;
+
+    while (true) {
+      pthread_mutex_unlock(&cache_mutex);
+
+      int r; auto ret = cl->call(lock_protocol::acquire, cl->id(), lid, seq, r);
+      if (ret == lock_protocol::OK) break;
+
+      pthread_mutex_lock(&cache_mutex);
+      // retry activated by outdated messages should be ignored
+      while (lock.seqnum_at_retry < lock.seqnum) {
+        pthread_cond_wait(&retry_signal, &cache_mutex);
+      }
+      // if the call fails we need to wait for another retry request
+      // doing this to enter the signal wait again
+      lock.seqnum_at_retry -= 1;
+    }
+
+    pthread_mutex_lock(&cache_mutex);
+    lock.status = Lock::LOCKED;
+    lock.seqnum_at_retry = lock.seqnum;
+    return lock_protocol::OK;
+  }
 }
+
+
 
 lock_protocol::status
-lock_client_cache::release(lock_protocol::lockid_t lid)
-{
-  return lock_protocol::RPCERR;
+lock_client_cache::release(lock_protocol::lockid_t lid) {
+  pthread_mutex_lock(&cache_mutex);
+
+  Lock& lock = cache[lid];
+  // release to cache
+  if (lock.seqnum_at_revoke < lock.seqnum) {
+    lock.status = Lock::FREE;
+    pthread_mutex_unlock(&cache_mutex);
+
+    pthread_cond_broadcast(&released_to_cache_signal);
+    return lock_protocol::OK;
+  }
+  // release to server
+  lock.status = Lock::RELEASING;
+  release_queue.add(release_req(lid, lock.seqnum));
+  return lock_protocol::OK;
 }
 
+
+
+rlock_protocol::status
+lock_client_cache::revoke_handler(lock_protocol::lockid_t lid, unsigned int seq, int& r) {
+  ScopedLock guard(&cache_mutex);
+
+  Lock& lock = cache[lid];
+  lock.seqnum_at_revoke = seq;
+
+  if (lock.status != Lock::FREE) {
+    return rlock_protocol::OK;
+  }
+  lock.status = Lock::RELEASING;
+  release_queue.add(release_req(lid, seq));
+  return rlock_protocol::OK;
+}
+
+
+
+rlock_protocol::status
+lock_client_cache::retry_handler(lock_protocol::lockid_t lid, unsigned int seq, int& r) {
+  pthread_mutex_lock(&cache_mutex);
+  cache[lid].seqnum_at_retry = seq;
+  pthread_mutex_unlock(&cache_mutex);
+
+  pthread_cond_broadcast(&retry_signal);
+  return rlock_protocol::OK;
+}
